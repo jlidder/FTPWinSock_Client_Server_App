482447dcbde9384bf062ca20e221b680a4339a1a
Jaspreet Singh Lidder <jaspreetsinghlidder@gmail.com>
2013-02-05 22:42:41 -0500

    code cleanup + some minor refactoring.

--- Client/Source/cli_tcp.cpp
+++ Client/Source/cli_tcp.cpp
@@ -61,28 +61,27 @@ char localhost[11],
 HANDLE test;
 DWORD dwtest;
 
-#define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
-						//reference for used structures
+//reference for used structures
 
-						/*  * Host structure
+/*  * Host structure
 
-							struct  hostent {
-							char    FAR * h_name;             official name of host *
-							char    FAR * FAR * h_aliases;    alias list *
-							short   h_addrtype;               host address type *
-							short   h_length;                 length of address *
-							char    FAR * FAR * h_addr_list;  list of addresses *
-						#define h_addr  h_addr_list[0]            address, for backward compat *
-						};
+struct  hostent {
+char    FAR * h_name;             official name of host *
+char    FAR * FAR * h_aliases;    alias list *
+short   h_addrtype;               host address type *
+short   h_length;                 length of address *
+char    FAR * FAR * h_addr_list;  list of addresses *
+#define h_addr  h_addr_list[0]            address, for backward compat *
+};
 
-						 * Socket address structure
+* Socket address structure
 
-						 struct sockaddr_in {
-						 short   sin_family;
-						 u_short sin_port;
-						 struct  in_addr sin_addr;
-						 char    sin_zero[8];
-						 }; */
+struct sockaddr_in {
+short   sin_family;
+u_short sin_port;
+struct  in_addr sin_addr;
+char    sin_zero[8];
+}; */
 
 //------------------------------------------------------------------------------------------------------------------------------------
 //FUNCTIONS TO BE USED BY MAIN()------------------------------------------------------------------------------------------------------
@@ -103,7 +102,6 @@ void WriteToFile(char * filename, char * data)
 //------------------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------------
-
 int main(void)
 {
 	WSADATA wsadata;
@@ -138,7 +136,6 @@ int main(void)
 
 		cout << "Type name of ftp server :" << flush ;   
 		cin >> remotehost ;
-		//cout << "Remote host name is: \"" << remotehost << "\"" << endl;
 		if((rp=gethostbyname(remotehost)) == NULL)
 			throw "remote gethostbyname failed\n";
 
@@ -162,10 +159,7 @@ int main(void)
 			throw "connect failed\n";
 
 		/* Have an open connection, so, server is 
-
-		   - waiting for the client request message
-		   - don't forget to append <carriage return> 
-		   - <line feed> characters after the send buffer to indicate end-of file */
+		   - don't forget to append <carriage return>  */
 
 		  ifstream::pos_type filesize;
 		  char * memblock;
@@ -198,100 +192,79 @@ int main(void)
 			char server_request_response[128];
 			if((ibytesrecv = recv(s,server_request_response,128,0)) == SOCKET_ERROR) // SERVER SHOULD RESPOND WITH "OK" RESPONSE TO GET REQUEST
 				throw "get failed\n";
-			else
+
+			std::string server_msg_converted( reinterpret_cast< char const* >(server_request_response) );
+			if(server_msg_converted=="ok")//server responded with OK. we can now prepare for file transfer.
 			{
-				std::string server_msg_converted( reinterpret_cast< char const* >(server_request_response) );
-				if(server_msg_converted=="ok")
+				if (send(s,filename, 128, 0) == SOCKET_ERROR)// TELL THE SERVER, THE FILE NAME TO BE TRANSFERED OVER.
+					throw "file transfer initiation failed\n";  
+
+				char num_of_data_chunks_to_receive[128];
+
+				if((ibytesrecv = recv(s,num_of_data_chunks_to_receive,128,0)) == SOCKET_ERROR) // SERVER SHOULD RESPOND WITH HOW MANY DATA CHUNKS IT HAS PREPARED TO SEND TO US. 
+					throw "Server could not receive";                                          // THIS WILL HELP US DEFINE A LOOP FOR CONTINIOUS RECEIVE AND SEND MSG'S.
+
+				//setup file chunks to be received
+				std::string data_chunk_string_form( reinterpret_cast< char const* >(num_of_data_chunks_to_receive) );
+				int data_chunk_int_form = atoi(data_chunk_string_form.c_str());
+
+				packet_collection = new PACKET[data_chunk_int_form]; //setting up the data collection
+
+				char start_msg[128];
+				sprintf_s(start_msg,"start");
+
+				if (send(s,start_msg, 128, 0) == SOCKET_ERROR)// TELL THE SERVER, THE FILE NAME TO BE TRANSFERED OVER.
+					throw "file transfer initiation failed\n";  
+
+				int total_bytes_of_file = data_chunk_int_form*1300;
+
+				for(int receive_loop=0; receive_loop < data_chunk_int_form; receive_loop++) // RECEIVE EACH DATA CHUNK
 				{
-					if (send(s,filename, 128, 0) == SOCKET_ERROR)// TELL THE SERVER, THE FILE NAME TO BE TRANSFERED OVER.
-						throw "file transfer initiation failed\n";  
+					/*
+					 * FILE RECEIVE LOGIC GOES HERE! (probably a for loop. LOOP(request <--> response) )
+					 */
+					char data_chunk[1300];
+					if((ibytesrecv = recv(s,data_chunk,1300,0)) == SOCKET_ERROR) // WAIT FOR THE DATA CHUNK TO BE SENT.
+						throw "get data failed\n";
 
-					else
+					for(int data_byte=0; data_byte<1300; data_byte++) //GO THROUGH EACH BYTE IN DATA CHUNK AND DEEP COPY IT OVER TO ITS RESPECTIVE PACKET.
 					{
-						char num_of_data_chunks_to_receive[128];
+						memcpy (&packet_collection[receive_loop].data[data_byte], &data_chunk[data_byte],1);
+						if(receive_loop==1299)
+							memcpy (&packet_collection[receive_loop].data[receive_loop+1], "\0" ,1);
+					}
+					
+					char receive_msg[128];
+					sprintf_s(receive_msg,"received");
 
-						if((ibytesrecv = recv(s,num_of_data_chunks_to_receive,128,0)) == SOCKET_ERROR) // SERVER SHOULD RESPOND WITH HOW MANY DATA CHUNKS IT HAS PREPARED TO SEND TO US. 
-							throw "Server could not receive";                                          // THIS WILL HELP US DEFINE A LOOP FOR CONTINIOUS RECEIVE AND SEND MSG'S.
-						else
-						{
-							//setup file chunks to be received
-							std::string data_chunk_string_form( reinterpret_cast< char const* >(num_of_data_chunks_to_receive) );
-							int data_chunk_int_form = atoi(data_chunk_string_form.c_str());
+					if (send(s,receive_msg, 128, 0) == SOCKET_ERROR)// TELL THE SERVER, THE FILE NAME TO BE TRANSFERED OVER.
+						throw "file transfer initiation failed\n";  
+				}//end of for-loop which waits for receiving each data chunk
 
-							packet_collection = new PACKET[data_chunk_int_form]; //setting up the data collection
+				//finished file transfer over. We need to marshal the data into its respective file type
+				char * file_data_buffer_TOBEWRITTEN = new char[total_bytes_of_file];
 
-							char start_msg[128];
-							sprintf_s(start_msg,"start");
+				//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+				//take all fragmented data and put it all together. (unify data chunks)
+				int marshalcounter=0;
 
-							if (send(s,start_msg, 128, 0) == SOCKET_ERROR)// TELL THE SERVER, THE FILE NAME TO BE TRANSFERED OVER.
-								throw "file transfer initiation failed\n";  
-
-							else
-							{
-								int total_bytes_of_file = data_chunk_int_form*1300;
-
-								for(int receive_loop=0; receive_loop < data_chunk_int_form; receive_loop++) // RECEIVE EACH DATA CHUNK
-								{
-									/*
-									 * FILE RECEIVE LOGIC GOES HERE! (probably a for loop. LOOP(request <--> response) )
-									 */
-									char data_chunk[1300];
-									if((ibytesrecv = recv(s,data_chunk,1300,0)) == SOCKET_ERROR) // WAIT FOR THE DATA CHUNK TO BE SENT.
-										throw "get data failed\n";
-									else
-									{
-										for(int data_byte=0; data_byte<1300; data_byte++) //GO THROUGH EACH BYTE IN DATA CHUNK AND DEEP COPY IT OVER TO ITS RESPECTIVE PACKET.
-										{
-											memcpy (&packet_collection[receive_loop].data[data_byte], &data_chunk[data_byte],1);
-											if(receive_loop==1299)
-												memcpy (&packet_collection[receive_loop].data[receive_loop+1], "\0" ,1);
-											continue;
-										}
-									}
-
-									//send a confirmation of receipt.....
-									char receive_msg[128];
-									sprintf_s(receive_msg,"received");
-
-									if (send(s,receive_msg, 128, 0) == SOCKET_ERROR)// TELL THE SERVER, THE FILE NAME TO BE TRANSFERED OVER.
-										throw "file transfer initiation failed\n";  
-									else
-										continue;
-									//-----------------
-								}//end of for-loop which waits for receiving each data chunk
-
-								//finished file transfer over. We need to marshal the data into its respective file type
-								char * file_data_buffer_TOBEWRITTEN = new char[total_bytes_of_file];
-
-								//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-								//sent all data into a single array.
-								//for(int marshalcounter=0 ; marshalcounter< total_bytes_of_file; marshalcounter++)
-								int marshalcounter=0;
-
-								for (int packet_collection_counter=0; packet_collection_counter < data_chunk_int_form; packet_collection_counter++)
-								{
-									for(int bytes_in_packet_counter=0;bytes_in_packet_counter<1300;bytes_in_packet_counter++)
-									{
-										memcpy (&file_data_buffer_TOBEWRITTEN[marshalcounter], &packet_collection[packet_collection_counter].data[bytes_in_packet_counter],1);
-										marshalcounter++;
-									}
-								}
-								//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-								//###################################################################################
-								//write bytes into file
-								WriteToFile(filename,file_data_buffer_TOBEWRITTEN);
-								//###################################################################################
-							}
-						}
+				for (int packet_collection_counter=0; packet_collection_counter < data_chunk_int_form; packet_collection_counter++)
+				{
+					for(int bytes_in_packet_counter=0;bytes_in_packet_counter<1300;bytes_in_packet_counter++)
+					{
+						memcpy (&file_data_buffer_TOBEWRITTEN[marshalcounter], &packet_collection[packet_collection_counter].data[bytes_in_packet_counter],1);
+						marshalcounter++;
 					}
 				}
-				else
-					throw "Server Denied request!";
+				//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+				//###################################################################################
+				//write bytes into file
+				WriteToFile(filename,file_data_buffer_TOBEWRITTEN);
+				//###################################################################################
 			}
-			cout << "reached end of client";
-			int testvar;
-			cin >> testvar;
+			else
+				throw "Server Denied request!";
 		  }
 		  //*******************************************************************************************************************************
 		  //*******************************************************************************************************************************
@@ -315,9 +288,9 @@ int main(void)
 	catch (char *str)
 	{
 		cerr<<str<<":"<<dec<<WSAGetLastError()<<endl;
-					cout << "reached end of client";
-			int testvar;
-			cin >> testvar;
+		cout << "error occured....";
+		int testvar;
+		cin >> testvar;
 	}
 
 	//close the client socket

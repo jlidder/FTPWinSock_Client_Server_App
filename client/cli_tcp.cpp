48c71082be5aa31a622853fab409491dfbfa5757
Jaspreet Singh Lidder <jaspreetsinghlidder@gmail.com>
2013-03-24 23:29:39 -0300

    ass2 submission.

--- client/cli_tcp.cpp
+++ client/cli_tcp.cpp
@@ -140,7 +140,7 @@ int main(void){
                     File_Names_Array_PTRS[i] = new char[128];
             }
 
-            sendNwait(msgFrame);
+            sendNwait();
 
             //will receive and ACK in this loop
             for(int counter=0; counter<list_size; counter++)
@@ -148,10 +148,10 @@ int main(void){
                     //string filename_rcvd( reinterpret_cast< char const* >(mgFrame.data) );
                     cout << msgFrame.data << endl;
                     sprintf(msgFrame.data,"rcvdname");
-                    sendNwait(msgFrame);
+                    sendNwait();
             }
 			sprintf(msgFrame.data,"donelist");
-			sendNwait(msgFrame);
+			sendNwait();
 		}
 
 		else if (msgFrame.command[0] == 'g' || msgFrame.command[0] == 'G')
@@ -162,12 +162,24 @@ int main(void){
 
 			else//receive file
 			{//calulate packet number
+
+				FILE * logFile2;
+				char name [100];
+				logFile2 = fopen ("log.txt","a");
+
+				ostringstream convert5;
+				convert5 << msgFrame.file_size;// insert the textual representation of 'Number' in the characters in the stream
+				string file_size_string = convert5.str(); // set 'Result' to the contents of the stream
+				char file_transfer_msg[200]="File transfer initiated(GET)\n";
+				fprintf(logFile2,file_transfer_msg);
+				fclose(logFile2);
+
 				packet_amount = (int)ceil(((double)msgFrame.file_size/PACKET_SIZE));
 				ofstream outFile( msgFrame.file_name, ios::binary | ios::out);
 
 				for(int i=1;i<=packet_amount;i++)//needs to use select for final implementation
 				{
-					sendNwait(msgFrame);
+					sendNwait();
 					if(i < packet_amount)
 					{
 						outFile.write(msgFrame.data,PACKET_SIZE);
@@ -190,11 +202,22 @@ int main(void){
 				if(inFile.is_open())
 				{
 
+					FILE * logFile2;
+					char name [100];
+					logFile2 = fopen ("log.txt","a");
+
+					ostringstream convert5;
+					convert5 << msgFrame.file_size;// insert the textual representation of 'Number' in the characters in the stream
+					string file_size_string = convert5.str(); // set 'Result' to the contents of the stream
+					char file_transfer_msg[200]="File transfer initiated(PUT)\n";
+					fprintf(logFile2,file_transfer_msg);
+					fclose(logFile2);
+
 					msgFrame.file_size = inFile.tellg();
 					inFile.seekg(0, ios::beg);
 
 					//sending file size
-					sendNwait(msgFrame);
+					sendNwait();
 
 					//sending file
 					int packet_amount = (int)ceil(((double)msgFrame.file_size/(double)PACKET_SIZE));
@@ -202,7 +225,7 @@ int main(void){
 					{
 						inFile.read(msgFrame.data,PACKET_SIZE);//read from file		
 						//send what is readsendNwait(msgFrame);
-						sendNwait(msgFrame);
+						sendNwait();
 					}
 				}
 				else
@@ -211,7 +234,7 @@ int main(void){
 					msgFrame.file_size = -1;
 						
 					//sending no file found -1
-					sendNwait(msgFrame);
+					sendNwait();
 					cout << "file doesn't exist";
 
 				}
@@ -220,8 +243,8 @@ int main(void){
 
 		else if (msgFrame.command[0] == 'd' || msgFrame.command[0] == 'D')
 		{
-			sprintf(msgFrame.data,hsFrame.file_name);
-			sendNwait(msgFrame);
+			sprintf(msgFrame.data,hldFrame.file_name);
+			sendNwait();
 
 			string delete_success_or_failure( reinterpret_cast< char const*>(msgFrame.data) );
 
@@ -244,10 +267,8 @@ int main(void){
 	return 0;
 }
 
-
 void hndShake()
-{///initialize to non seqNo
-
+{
 	//preparing log stuff.
 	FILE * logFile;
 	char name [100];
@@ -259,23 +280,32 @@ void hndShake()
 
 	fprintf(logFile,log_msg_3wayhandshake);
 
+	///initialize to non seqNo no
 	msgFrame.client_seqNo = -1;
 	msgFrame.server_seqNo = -1;
-	int serverNo = rand() % 256; //rand number 0 to 255
-	msgFrame.client_number = serverNo;
+	msgFrame.client_number = -1;
+	msgFrame.server_number = -1;
+	int clientNo = rand() % 256; //rand number 0 to 255
+	msgFrame.client_number = clientNo;
 	cout<<"Sending " << msgFrame.client_number;
 	do
 	{//resends packet until it receives responds
+		char log_msg_sending[128] = "Client: sent packet: ";
+		char log_msg_receiving[128] = "Client: received ACK for packet: ";
+		char log_msg_3wayhandshake[128] = "Client: 3 way handshaking beginning\n";
+		char log_msg_3wayhandshake_finished[128] = "Client: 3 way handshaking completed! \n";
 
-		string pack_num_string;          // string which will contain the result
-		ostringstream convert;   // stream used for the conversion
-		convert << msgFrame.client_number;// insert the textual representation of 'Number' in the characters in the stream
-		pack_num_string = convert.str(); // set 'Result' to the contents of the stream
+		if((ibytessent = sendto(s,(char*)&msgFrame,sizeof(msgFrame),0,(sockaddr *)&sa_in, sizeof(sa_in)))==SOCKET_ERROR)//initial hs with client Number
+					{throw "error in send in server program\n";}
+
+		//log the packet sent in log file.
+		string pack_num_string;          
+		ostringstream convert;  
+		convert << msgFrame.client_number;
+		pack_num_string = convert.str();
 		fprintf(logFile,strcat(log_msg_sending,pack_num_string.c_str()));
 	    fprintf(logFile,"\n");
 
-		if((ibytessent = sendto(s,(char*)&msgFrame,sizeof(msgFrame),0,(sockaddr *)&sa_in, sizeof(sa_in)))==SOCKET_ERROR)
-					{throw "error in send in server program\n";}
 		struct timeval tp;
 		tp.tv_usec = 0;
 		tp.tv_sec=1;
@@ -285,8 +315,8 @@ void hndShake()
 		 {exit(EXIT_FAILURE);}
 		else if(RetVal > 0)
 		{//it now will wait for the msgFrame to begin data tranfer
-			if((ibytesrecv = recvfrom(s,(char*) &msgFrame,sizeof(msgFrame),0, (SOCKADDR *) & SenderAddr, &SenderAddrSize)) == SOCKET_ERROR)
-				throw "Receive failed\n";
+			if((ibytesrecv = recvfrom(s,(char*) &msgFrame,sizeof(msgFrame),0, (SOCKADDR *) & SenderAddr, &SenderAddrSize)) == SOCKET_ERROR)//wating for server to reply with servernumber&
+				throw "Receive failed\n";																								//client number
 
 			ostringstream convert1;
 			convert1 << msgFrame.server_number;// insert the textual representation of 'Number' in the characters in the stream
@@ -294,28 +324,36 @@ void hndShake()
 			fprintf(logFile,strcat(log_msg_receiving,pack_num_string.c_str()));
 			fprintf(logFile,"\n");
 		}
-	}while (RetVal ==0 ||serverNo != msgFrame.client_number);	
+	}while (RetVal ==0 ||msgFrame.server_number <0);	
+
 	cout << " Received " << msgFrame.server_number<<endl;
-	msgFrame.client_seqNo = msgFrame.client_number % 2;//sets the sequence number
+	msgFrame.client_seqNo = msgFrame.client_number % 2;//create client sequence number
 	
 	cout<<"Handshake complete";
-	int seqNo = msgFrame.server_seqNo;
+	//making a deep copy of original values in case of resending
+	hldFrame.client_number = msgFrame.client_number; hldFrame.server_number = msgFrame.server_number; hldFrame.client_seqNo = msgFrame.client_seqNo;hldFrame.server_seqNo=msgFrame.server_seqNo;
+	sprintf(hldFrame.command,msgFrame.command);sprintf(hldFrame.file_name,msgFrame.file_name);hldFrame.file_size=msgFrame.file_size;
+	memcpy(hldFrame.data,msgFrame.data,PACKET_SIZE);
+	bool wrongPacket = false;//control bool
+	//loop for resending if not received
+		do 
+	{//waits for server first packet reply for action selected with servers packet number
+		ibytessent = sendto(s,(char*)&msgFrame,sizeof(msgFrame),0,(sockaddr *)&sa_in, sizeof(sa_in));//send client_aeq
+		if (ibytessent == SOCKET_ERROR)
+			throw "Send failed\n";
 
-	do
-	{
-		string pack_num_string1;          // string which will contain the result
-		ostringstream convert2;   // stream used for the conversion
-		convert2 << msgFrame.client_number;// insert the textual representation of 'Number' in the characters in the stream
-		pack_num_string1 = convert2.str(); // set 'Result' to the contents of the stream
+		//log the send
+		string pack_num_string1;         
+		ostringstream convert2;  
+		convert2 << msgFrame.client_number;
+		pack_num_string1 = convert2.str(); 
 		fprintf(logFile,strcat(log_msg_sending,pack_num_string1.c_str()));
 	    fprintf(logFile,"\n");
 
-		ibytessent = sendto(s,(char*)&msgFrame,sizeof(msgFrame),0,(sockaddr *)&sa_in, sizeof(sa_in));
-		if (ibytessent == SOCKET_ERROR)
-			throw "Send failed\n";
+
 		struct timeval tp;
-		tp.tv_usec =0;
-		tp.tv_sec=1;
+		tp.tv_usec =300000;
+		tp.tv_sec=0;
 		FD_ZERO(&readfds); //initialize 
 		FD_SET(s, &readfds); //put the socket in the set
 		if((RetVal = select (1 , &readfds, NULL, NULL, &tp))==SOCKET_ERROR) 
@@ -323,66 +361,90 @@ void hndShake()
 		else if(RetVal > 0)
 		{
 			if((ibytesrecv = recvfrom(s,(char*) &msgFrame,sizeof(msgFrame),0, (SOCKADDR *) & SenderAddr, &SenderAddrSize)) == SOCKET_ERROR)
-				throw "Receive failed\n";
+				throw "Receive failed\n";//waiting for server seq
 
 			ostringstream convert3;
-			convert3 << msgFrame.server_number;// insert the textual representation of 'Number' in the characters in the stream
-			string pack_num_string2 = convert3.str(); // set 'Result' to the contents of the stream
+			convert3 << msgFrame.server_number;
+			string pack_num_string2 = convert3.str(); 
 			fprintf(logFile,strcat(log_msg_receiving,pack_num_string2.c_str()));
 			fprintf(logFile,"\n");
-		}	
-	}while (RetVal ==0 ||(msgFrame.server_seqNo==-1));
+			
+			if((msgFrame.server_seqNo<0))
+			{	//reverse msgFrame by deep copy and discard packet
+				msgFrame.client_number=hldFrame.client_number;msgFrame.server_number=hldFrame.server_number;msgFrame.client_seqNo=hldFrame.client_seqNo;msgFrame.server_seqNo=hldFrame.server_seqNo;
+				sprintf(msgFrame.command,hldFrame.command);sprintf(msgFrame.file_name,hldFrame.file_name);msgFrame.file_size=hldFrame.file_size;
+				memcpy(msgFrame.data,hldFrame.data,PACKET_SIZE);
+				wrongPacket = true; // set the wrongPacket as true
+			}
+			else
+				wrongPacket = false;
+		}
+	}while (RetVal ==0 ||wrongPacket);	
 
 	fprintf(logFile,log_msg_3wayhandshake_finished);
 	fclose(logFile);
 }
 
+
 //Stop and Wait functions
- void sendNwait(MESSAGE_FRAME &buff)
+ void sendNwait()
 {//sends and waits for reply. If time out resends
 	FILE * logFile;
 	char name [100];
 	logFile = fopen ("log.txt","a");
 
-	int seqNo = buff.server_seqNo;
-	int newSeq = (msgFrame.server_seqNo+1) %2;
-	msgFrame.server_seqNo=newSeq;
+	msgFrame.server_seqNo = (msgFrame.server_seqNo+1) %2;//increase server seqNo
+	//making a deep copy of original values in case of resending
+	hldFrame.client_number = msgFrame.client_number; hldFrame.server_number = msgFrame.server_number; hldFrame.client_seqNo = msgFrame.client_seqNo;hldFrame.server_seqNo=msgFrame.server_seqNo;
+	sprintf(hldFrame.command,msgFrame.command);sprintf(hldFrame.file_name,msgFrame.file_name);hldFrame.file_size=msgFrame.file_size;
+	memcpy(hldFrame.data,msgFrame.data,PACKET_SIZE);
+	bool wrongPacket = false;//control bool
 	do
-	{
-		ibytessent = sendto(s,(char*)&buff,sizeof(buff),0,(sockaddr *)&sa_in, sizeof(sa_in));
+	{//waits and resends if it times out.  Also resends if same packet is received
+		ibytessent = sendto(s,(char*)&msgFrame,sizeof(msgFrame),0,(sockaddr *)&sa_in, sizeof(sa_in));
 		if (ibytessent == SOCKET_ERROR)
 			throw "Send failed\n";
 
 		char log_msg_sending[128] = "Client: sent packet: ";
 		char log_msg_receiving[128] = "Client: received ACK for packet: ";
 
-		string pack_num_string;          // string which will contain the result
-		ostringstream convert;   // stream used for the conversion
-		convert << msgFrame.client_seqNo;// insert the textual representation of 'Number' in the characters in the stream
-		pack_num_string = convert.str(); // set 'Result' to the contents of the stream
+		string pack_num_string;         
+		ostringstream convert; 
+		convert << msgFrame.client_seqNo;
+		pack_num_string = convert.str(); 
 		fprintf(logFile,strcat(log_msg_sending,pack_num_string.c_str()));
 	    fprintf(logFile,"\n");
 
 		struct timeval tp;
-		tp.tv_usec =0;
-		tp.tv_sec=1;
+		tp.tv_usec =300000;//time out
+		tp.tv_sec=0;
 		FD_ZERO(&readfds); //initialize 
 		FD_SET(s, &readfds); //put the socket in the set
 		if((RetVal = select (1 , &readfds, NULL, NULL, &tp))==SOCKET_ERROR) 
 		 {exit(EXIT_FAILURE);}
 		else if(RetVal > 0)
 		{
-			if((ibytesrecv = recvfrom(s,(char*) &buff,sizeof(buff),0, (SOCKADDR *) & SenderAddr, &SenderAddrSize)) == SOCKET_ERROR)
-				throw "Receive failed\n";
-
-			string pack_num_string;          // string which will contain the result
-			ostringstream convert;   // stream used for the conversion
-			convert << msgFrame.server_seqNo;// insert the textual representation of 'Number' in the characters in the stream
-			pack_num_string = convert.str(); // set 'Result' to the contents of the stream
+			if((ibytesrecv = recvfrom(s,(char*) &msgFrame,sizeof(msgFrame),0, (SOCKADDR *) & SenderAddr, &SenderAddrSize)) == SOCKET_ERROR)
+				throw "Receive failed\n";		
+			if(hldFrame.client_seqNo == msgFrame.client_seqNo)
+			{
+				//reverse msgFrame by deep copy ready to send again
+				msgFrame.client_number=hldFrame.client_number;msgFrame.server_number=hldFrame.server_number;msgFrame.client_seqNo=hldFrame.client_seqNo;msgFrame.server_seqNo=hldFrame.server_seqNo;
+				sprintf(msgFrame.command,hldFrame.command);sprintf(msgFrame.file_name,hldFrame.file_name);msgFrame.file_size=hldFrame.file_size;
+				memcpy(msgFrame.data,hldFrame.data,PACKET_SIZE);
+				wrongPacket = true; // set the wrongPacket as true
+			}
+			else
+				wrongPacket = false;
+			
+			string pack_num_string;         
+			ostringstream convert;   
+			convert << msgFrame.server_seqNo;
+			pack_num_string = convert.str(); 
 			fprintf(logFile,strcat(log_msg_receiving,pack_num_string.c_str()));
 			fprintf(logFile,"\n");
 		}	
-	}while (RetVal ==0 || (seqNo == buff.server_seqNo));	
+	}while (RetVal ==0 || wrongPacket);
 
 	fclose(logFile);
 }
